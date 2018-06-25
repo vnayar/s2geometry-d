4d1504922f89ce47866260c1ac7d66da1625b5c8
Vijay Nayar <madric@gmail.com>
2018-06-25 23:40:39 +0200

    Wrapped up tests for rbtree_map.

--- src/s2/util/container/rbtree_map.d
+++ src/s2/util/container/rbtree_map.d
@@ -3,54 +3,73 @@ module s2.util.container.rbtree_map;
 import std.container.rbtree;
 import std.algorithm : map;
 import std.functional : binaryFun;
-import std.traits : isImplicitlyConvertible;
+import std.meta : allSatisfy;
+import std.range : ElementType, isInputRange;
+import std.traits : isDynamicArray, isImplicitlyConvertible;
 
 /**
  * A dictionary or associative array backed by a Red-Black tree.
  */
 final class RBTreeMap(KeyT, ValueT, alias KeyLessF = "a < b", bool allowDuplicates = false) {
-private:
+public:
   static struct Pair {
     KeyT key;
     ValueT value;
   }
+
   alias keyLess = binaryFun!KeyLessF;
 
   alias RedBlackTreeT =
       RedBlackTree!(Pair, (pair1, pair2) => keyLess(pair1.key, pair2.key), allowDuplicates);
 
-public:
-  RedBlackTreeT rbtree;
+  RedBlackTreeT rbTree;
 
   // Forward compatible methods like: empty(), length(), opSlice(), etc.
-  alias rbtree this;
+  alias rbTree this;
 
   this() {
-    rbtree = new RedBlackTreeT();
+    rbTree = new RedBlackTreeT();
+  }
+
+  this(Pair[] elems...) {
+    rbTree = new RedBlackTreeT(elems);
+  }
+
+  this(PairRange)(PairRange pairRange)
+  if (isInputRange!PairRange && isImplicitlyConvertible!(ElementType!PairRange, Pair)) {
+    rbTree = new RedBlackTreeT(pairRange);
+  }
+
+  override
+  bool opEquals(Object rhs) {
+    RBTreeMap that = cast(RBTreeMap) rhs;
+    if (that is null) return false;
+
+    return rbTree == that.rbTree;
   }
 
   /// Insertion
   size_t stableInsert(K, V)(K key, V value)
   if (isImplicitlyConvertible!(K, KeyT) && isImplicitlyConvertible!(V, ValueT)) {
-    return rbtree.stableInsert(Pair(key, value));
+    return rbTree.stableInsert(Pair(key, value));
   }
   alias insert = stableInsert;
 
   ValueT opIndexAssign(ValueT value, KeyT key) {
-    rbtree.stableInsert(Pair(key, value));
+    rbTree.stableInsert(Pair(key, value));
     return value;
   }
 
   /// Membership
   bool opBinaryRight(string op)(KeyT key) const
   if (op == "in") {
-    return Pair(key) in rbtree;
+    return Pair(key) in rbTree;
   }
 
   /// Removal
   size_t removeKey(K...)(K keys)
   if (allSatisfy!(isImplicitlyConvertibleToKey, K)) {
-    KeyT[U.length] toRemove = [keys];
+    KeyT[K.length] toRemove = [keys];
     return removeKey(toRemove[]);
   }
 
@@ -62,8 +81,8 @@ public:
 
   size_t removeKey(K)(K[] keys)
   if (isImplicitlyConvertible!(K, KeyT)) {
-    auto keyPairs = keys.map(key => Pair(key));
-    return rbtree.removeKey(keyPairs);
+    auto keyPairs = keys.map!(key => Pair(key));
+    return rbTree.removeKey(keyPairs);
   }
 
   size_t removeKey(KeyRange)(KeyRange keyRange)
@@ -71,36 +90,36 @@ public:
       && isImplicitlyConvertible!(ElementType!KeyRange, KeyT)
       && !isDynamicArray!KeyRange) {
     auto keyPairs = keys.map(key => Pair(key));
-    return rbtree.removeKey(keyPairs);
+    return rbTree.removeKey(keyPairs);
   }
 
   /// Ranges
-  auto /*RedBlackTreeT.Range*/ upperBound(KeyT key) {
-    return rbtree.upperBound(Pair(key));
+  RedBlackTreeT.Range upperBound(KeyT key) {
+    return rbTree.upperBound(Pair(key));
   }
 
-  auto /*RedBlackTree.ConstRange*/ upperBound(KeyT key) const {
-    return rbtree.upperBound(Pair(key));
+  RedBlackTreeT.ConstRange upperBound(KeyT key) const {
+    return rbTree.upperBound(Pair(key));
   }
 
-  auto /*RedBlackTree.ImmutableRange*/ upperBound(KeyT key) immutable {
-    return rbtree.upperBound(Pair(key));
+  RedBlackTreeT.ImmutableRange upperBound(KeyT key) immutable {
+    return rbTree.upperBound(Pair(key));
   }
 
-  auto /*RedBlackTreeT.Range*/ lowerBound(KeyT key) {
-    return rbtree.lowerBound(Pair(key));
+  RedBlackTreeT.Range lowerBound(KeyT key) {
+    return rbTree.lowerBound(Pair(key));
   }
 
-  auto /*RedBlackTreeT.ConstRange*/ lowerBound(KeyT key) const {
-    return rbtree.lowerBound(Pair(key));
+  RedBlackTreeT.ConstRange lowerBound(KeyT key) const {
+    return rbTree.lowerBound(Pair(key));
   }
 
-  auto /*RedBlackTreeT.ImmutableRange*/ lowerBound(KeyT key) immutable {
-    return rbtree.lowerBound(Pair(key));
+  RedBlackTreeT.ImmutableRange lowerBound(KeyT key) immutable {
+    return rbTree.lowerBound(Pair(key));
   }
 
-  auto equalRange(this This)(Key key) {
-    return rbtree.equalRange(key);
+  auto equalRange(KeyT key) {
+    return rbTree.equalRange(Pair(key));
   }
 
 }
--- test/s2/util/container/rbtree_map_test.d
+++ test/s2/util/container/rbtree_map_test.d
@@ -12,4 +12,95 @@ unittest {
   rbTreeMap.insert("d", 1);
 
   Assert.equal(rbTreeMap.length(), 4);
+
+  auto rbTreeMap2 = new RBTreeMap!(string, int)();
+  rbTreeMap2["a"] = 4;
+  rbTreeMap2["b"] = 2;
+  rbTreeMap2["c"] = 3;
+  rbTreeMap2["d"] = 1;
+
+  Assert.equal(rbTreeMap2.length(), 4);
+
+  Assert.equal(rbTreeMap, rbTreeMap2);
+}
+
+@("membership")
+unittest {
+  alias RBTreeMapT = RBTreeMap!(string, int);
+  auto rbTreeMap = new RBTreeMapT([
+    RBTreeMapT.Pair("a", 4),
+    RBTreeMapT.Pair("b", 2),
+    RBTreeMapT.Pair("c", 3),
+    RBTreeMapT.Pair("d", 1)
+  ]);
+
+  assert("b" in rbTreeMap);
+  assert("d" in rbTreeMap);
+  assert("e" !in rbTreeMap);
+}
+
+@("removal")
+unittest {
+  auto rbTreeMap = new RBTreeMap!(string, int)();
+  rbTreeMap["a"] = 4;
+  rbTreeMap["b"] = 2;
+  rbTreeMap["c"] = 3;
+  rbTreeMap["d"] = 1;
+
+  rbTreeMap.removeKey("b");
+  rbTreeMap.removeKey("c");
+
+  Assert.equal(rbTreeMap.length(), 2);
+}
+
+@("lowerBound")
+unittest {
+  auto rbTreeMap = new RBTreeMap!(string, int)();
+  rbTreeMap["a"] = 4;
+  rbTreeMap["b"] = 2;
+  rbTreeMap["c"] = 3;
+  rbTreeMap["d"] = 1;
+
+  auto pairRange = rbTreeMap.lowerBound("c");
+  Assert.equal(pairRange.front(), rbTreeMap.Pair("a", 4));
+  pairRange.popFront();
+  Assert.equal(pairRange.empty(), false);
+  Assert.equal(pairRange.front(), rbTreeMap.Pair("b", 2));
+  pairRange.popFront();
+  Assert.equal(pairRange.empty(), true);
+}
+
+@("equalRange")
+unittest {
+  auto rbTreeMap = new RBTreeMap!(string, int, "a < b", true)();
+  rbTreeMap["a"] = 4;
+  rbTreeMap["b"] = 2;
+  rbTreeMap["c"] = 3;
+  rbTreeMap["d"] = 1;
+  rbTreeMap["b"] = 5;
+
+  auto pairRange = rbTreeMap.equalRange("b");
+  Assert.equal(pairRange.front(), rbTreeMap.Pair("b", 2));
+  pairRange.popFront();
+  Assert.equal(pairRange.empty(), false);
+  Assert.equal(pairRange.front(), rbTreeMap.Pair("b", 5));
+  pairRange.popFront();
+  Assert.equal(pairRange.empty(), true);
+}
+
+@("upperBound")
+unittest {
+  auto rbTreeMap = new RBTreeMap!(string, int)();
+  rbTreeMap["a"] = 4;
+  rbTreeMap["b"] = 2;
+  rbTreeMap["c"] = 3;
+  rbTreeMap["d"] = 1;
+
+  auto pairRange = rbTreeMap.upperBound("b");
+  Assert.equal(pairRange.front(), rbTreeMap.Pair("c", 3));
+  pairRange.popFront();
+  Assert.equal(pairRange.empty(), false);
+  Assert.equal(pairRange.front(), rbTreeMap.Pair("d", 1));
+  pairRange.popFront();
+  Assert.equal(pairRange.empty(), true);
 }
