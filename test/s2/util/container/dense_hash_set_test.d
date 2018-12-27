168b48a380c15bb401badb0538e908563a511885
Vijay Nayar <madric@gmail.com>
2018-12-27 01:06:59 +0100

    Include the actual tests.

---
+++ test/s2/util/container/dense_hash_set_test.d
@@ -0,0 +1,51 @@
+module s2.util.container.dense_hash_set_test;
+
+import s2.util.container.dense_hash_set;
+import fluent.asserts;
+
+@("int-set-insert") unittest {
+  auto intSet = new DenseHashSet!int();
+
+  intSet.setEmptyKey(-1);
+  intSet.insert(4);
+  intSet.insert(7);
+  intSet.insert(5);
+  Assert.equal(intSet.size(), 3);
+}
+
+@("char-set-insert") unittest {
+  auto charSet = new DenseHashSet!char();
+
+  charSet.setEmptyKey(cast(char) 255);
+  charSet.insert('c');
+  charSet.insert('o');
+  charSet.insert('w');
+  Assert.equal(charSet.size(), 3);
+}
+
+@("int-set-find") unittest {
+  auto hs = new DenseHashSet!int();
+  hs.setEmptyKey(-1);
+  hs.insert(3);
+  hs.insert(5);
+  hs.insert(8);
+  auto it = hs.find(5);  // Iterator is returned to find an element.
+  Assert.equal(*it, 5);  // Iterator's value matches the search target.
+  it = hs.find(10);      // Iterators are re-assignable with no error.
+  Assert.equal(it, hs.end());  // Find for non-existent elements returns end().
+}
+
+@("int-set-erase") unittest {
+  auto hs = new DenseHashSet!int();
+  hs.setEmptyKey(-1);
+  hs.setDeletedKey(-2);
+  hs.insert(3);
+  hs.insert(5);
+  hs.insert(8);
+
+  Assert.equal(hs.erase(5), 1);  // Erase returns the number of items removed.
+  Assert.equal(hs.erase(5), 0);  // Test duplicate erase.
+  Assert.equal(hs.erase(7), 0);  // Test erase of non-existent element.
+  Assert.equal(hs.size(), 2);    // Verify the new size of the DenseHashTable.
+}
+
