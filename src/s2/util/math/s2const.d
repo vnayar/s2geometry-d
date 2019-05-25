e267472716911aadb79a1493131e3a96bed31f65
Vijay Nayar <madric@gmail.com>
2019-05-25 13:03:00 +0200

    Add missing s2const.d.

---
+++ src/s2/util/math/s2const.d
@@ -0,0 +1,9 @@
+module s2.util.math.s2const;
+
+import std.math;
+
+enum double M_PI = cast(double) PI;
+enum double M_PI_2 = cast(double) PI_2;
+enum double M_PI_4 = cast(double) PI_4;
+enum double M_SQRT1_2 = cast(double) SQRT1_2;
+enum double M_SQRT2 = cast(double) SQRT2;
